613a923b3aa1f28faf633229125f82795295a8db
Durward Craig Jr <Durward.CraigJr@outlook.com>
2015-09-17 20:57:44 +0300

    Created a mini map

---
+++ Unity/Assets/Scripts/UI/MiniMap.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MiniMap : MonoBehaviour {
+
+    public float cameraHeight;
+    Vector3 cameraVector;
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        cameraVector = new Vector3(0.0f, cameraHeight, 0.0f);
+        gameObject.transform.position = GameObject.Find("Player").transform.position + cameraVector;
+    }
+}
