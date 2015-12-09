7e290f81630e8c4b571e4d234094b9bca77b405b
Durward Craig Jr <Durward.CraigJr@outlook.com>
2015-12-10 01:18:55 +0300

    Added handles for player equipment
    Handles are on both hands and back
    Added a scene view follower script
    Tweaked animator controller
    Tweaked PlayerController

--- Unity/Assets/Scripts/Player/PlayerController.cs
+++ Unity/Assets/Scripts/Player/PlayerController.cs
@@ -14,7 +14,6 @@ public class PlayerController : MonoBehaviour
     private InputManager inputs; //Used to create instance of Input Manager
     private Rigidbody rb;        //Handles the rigidbody component
     private Animator anim;       //Handles the animator component
-    private Animation clip;
 
     private Vector3 movePosition;   //Tracks direction of player movement
     private Vector3 moveRotation;   //Tracks direction of player rotation
@@ -26,7 +25,7 @@ public class PlayerController : MonoBehaviour
     private bool back;      //Track state of back key
     private bool left;      //Track state of left key
     private bool right;     //Track state of right key
-    private bool ctrl;
+    private bool ctrl;      //Track state of control key
 
     private bool mode;      //Tracks state of combat/free mode key
     private bool roll;      //Track state of roll key
@@ -50,12 +49,12 @@ public class PlayerController : MonoBehaviour
         back    = Input.GetKey(inputs.Backward);    //Get back key state
         left    = Input.GetKey(inputs.Left);        //Get left key state
         right   = Input.GetKey(inputs.Right);       //Get right key state
-        ctrl    = Input.GetKeyDown(inputs.Mode);
-        roll    = Input.GetKeyDown(inputs.Roll);
+        ctrl    = Input.GetKeyDown(inputs.Mode);    //Get combat key state
+        roll    = Input.GetKeyDown(inputs.Roll);    //Get roll key state
 
         float HorizontalRotate  = Input.GetAxis("Mouse X");     //Get horizontal mouse movement
         float VerticalRotate    = Input.GetAxis("Mouse Y");     //Get vertical mouse movement
-        float zoom = Input.GetAxis("Mouse ScrollWheel");
+        float zoom = Input.GetAxis("Mouse ScrollWheel");        //Get mouse scroll movement
 
         //Rotate player based on horizontal mouse movement multiplied by rotationSpeed
         moveRotation += new Vector3(0.0f, HorizontalRotate, 0.0f) * rotationSpeed * 10 * Time.deltaTime;
@@ -96,27 +95,24 @@ public class PlayerController : MonoBehaviour
             movePosition += transform.right * movement; //Add right transform from movePosition
         }
 
-        if (roll)
+        if (roll)   //If roll key has been pressed
         {
-            setState("Rolling", true);
-            StartCoroutine(Roll());
+            setState("Rolling", true);                  //Execute "Roll" animation
+            StartCoroutine(Roll());                     //Reset animation to false after playing
         }
 
         if (ctrl)  //If mode key is being held
         {
             changeMode(mode = !mode);       //Switch modes
             if (!mode)
-                movement = speed;
+                movement = speed;           //Adjust speed for free mode
             else
-                movement = combatspeed;
+                movement = combatspeed;     //Adjust speed for combat mode
         }
 
-        setMovementState(forward, back, left, right);                       //Update animator based on movement
+        setMovementState(forward, back, left, right);       //Update animator based on movement
+        rb.MovePosition(movePosition * Time.deltaTime);     //Move player through rigidbody(free)
 
-        if (!mode)
-            rb.MovePosition(movePosition * Time.deltaTime);         //Move player through rigidbody(free)
-        else
-            rb.MovePosition(movePosition * Time.deltaTime);   //Move player through rigidbody(combat)
 
     }
 
@@ -132,7 +128,7 @@ public class PlayerController : MonoBehaviour
         movePosition = transform.position;  //Set movePosition to player's initial position
         anim.SetBool("Empty", true);        //Player initially has no weapon
 
-        movement = speed;
+        movement = speed;                   //Set movement speed == free mode speed
     }
     #endregion
 
@@ -144,34 +140,36 @@ public class PlayerController : MonoBehaviour
         //Change weapon animations based on current weapon held
     void changeWeaponAnimation(string currentstate, string newstate)
     {
-        anim.SetBool(currentstate, false);
-        anim.SetBool(newstate, true);
+        anim.SetBool(currentstate, false);  //Current weapon state is now false
+        anim.SetBool(newstate, true);       //New weapon state is now true
     }
 
-        //Plays appropriate animations
+        //Adjusts parameters for movement
     void setMovementState(bool forward, bool back, bool left, bool right)
     {
-        anim.SetBool("Forward Key", forward);
-        anim.SetBool("Backward Key", back);
-        anim.SetBool("Left Key", left);
-        anim.SetBool("Right Key", right);
+        anim.SetBool("Forward Key", forward);   //Set forward key parameter
+        anim.SetBool("Backward Key", back);     //Set back key parameter
+        anim.SetBool("Left Key", left);         //Set left key parameter
+        anim.SetBool("Right Key", right);       //Set right key parameter
     }
 
+        //Change state of parameters in the Animator
     void setState(string parameter, bool state)
     {
-        anim.SetBool(parameter, state);
+        anim.SetBool(parameter, state);         //Set parameter == to state
     }
         //Change between combat and free modes
     void changeMode(bool mode)
     {
-        anim.SetBool("Mode", mode);
+        anim.SetBool("Mode", mode);             //Set mode == to mode
     }
 
+        //Coroutine for roll animation
     IEnumerator Roll()
     {
-        AnimatorStateInfo info = anim.GetCurrentAnimatorStateInfo(0);
-        yield return new WaitForSeconds(info.length);
-        setState("Rolling", false);
+        AnimatorStateInfo info = anim.GetCurrentAnimatorStateInfo(0);   //Get roll animation
+        yield return new WaitForSeconds(info.length);                   //Wait for roll length
+        setState("Rolling", false);                                     //Set state to false
     }
     #endregion
 }
